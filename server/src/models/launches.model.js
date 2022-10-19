f0d384190fbc84477d7341b0040c97b1f6aa0f67
luisa garrozi <luisagarrozi@gmail.com>
2022-10-19 08:43:48 -0300

    create launches using post

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -17,6 +19,23 @@ function getAllLaunches() {
   return Array.from(launches.values())
 }
 
+function addNewLaunch(launch) {
+  // increment flight number by 1, so the client doesn't need to keep track of that
+  latestFlightNumber++; 
+  // object.assign will assign a few additional properties to our launch object.
+  // If any properties are both in the launch object and the new object, the new object will override the existing properties
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['ZTM', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  ) 
+}
+
 module.exports = {
   getAllLaunches,
-};
+  addNewLaunch,
+}; 
